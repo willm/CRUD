1fae764d5ec00560b74cc76dc151bc3278b50573
Will <will@willubuntu.(none)>
2012-06-29 17:53:00 +0100

    first test

---
+++ src/Crud.Tests/Redis_persistence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BookSleeve;
+using NUnit.Framework;
+
+namespace Crud.Tests
+{
+	[TestFixture]
+	public class Redis_persistence
+	{
+		[Test]
+		public void can_save()
+		{
+			using(var connection = new RedisConnection("localhost")){
+				connection.Open();
+				connection.Strings.Set(1, "title", "bobby's tune");
+			}
+			
+			using(var connection = new RedisConnection("localhost")){
+				var result = connection.Strings.Get (1, "title");
+				Assert.That (result, Is.EqualTo("bobby's tune"));
+			}
+		}
+	}
+}
