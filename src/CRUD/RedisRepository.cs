a8f661706bf5e32e42d9b8afca353d20115bdd03
Will Munn <willmunn@hotmail.com>
2012-06-30 01:49:30 +0100

    Basic Redis CRUD

--- src/CRUD/RedisRepository.cs
+++ src/CRUD/RedisRepository.cs
@@ -5,13 +5,26 @@ namespace CRUD
 {
 	public class RedisRepository
 	{
+		private RedisConnection _connection;
+		
+		public RedisRepository (RedisConnection connection)
+		{
+			_connection = connection;
+		}
+		
 		private static int DB = 1;
 		
-		public void Save(string key, string value, RedisConnection connection){
-			//using(var connection = new RedisConnection("localhost")){
-				//connection.Open();
-				connection.Strings.Set(DB, key, value);
-			//}
+		public void Save(string key, string value){
+				_connection.Strings.Set(DB, key, value);
+		}
+		
+		public string Get(string key){
+			var result = _connection.Strings.GetString(DB, key);
+			return _connection.Wait(result);
+		}
+		
+		public void Delete(string key){
+			_connection.Keys.Remove(DB, key);
 		}
 	}
 }
--- src/Crud.Tests/Redis_persistence.cs
+++ src/Crud.Tests/Redis_persistence.cs
@@ -14,27 +14,58 @@ namespace Crud.Tests
 		string expectedString = "bobby's tune";
 		string key = "title";
 		int db = 1;
-		RedisRepository subject = new RedisRepository();
+		RedisRepository subject;
+		RedisConnection connection;
 		
 		[SetUp]
 		[TearDown]
 		public void ClearData(){
-			using(var con = new RedisConnection("localhost")){
-				con.Open();
-				con.Keys.Remove(db, key).Wait(2);
-			}
+			connection = new RedisConnection("localhost");
+			subject = new RedisRepository(connection);
+				connection.Open();
+				connection.Keys.Remove(db, key).Wait(2);
 		}		
 		
 		[Test]
 		public void can_save()
 		{			
-			using(var connection = new RedisConnection("localhost")){
-				connection.Open();
-				subject.Save(key,expectedString, connection);	
-				var result = connection.Strings.GetString (db, key);
-				
-				Assert.That (connection.Wait(result), Is.EqualTo(expectedString));
-			}
+			subject.Save(key,expectedString);	
+			var result = connection.Strings.GetString (db, key);
+			
+			Assert.That (connection.Wait(result), Is.EqualTo(expectedString));
+		}
+		
+		[Test]
+		public void can_update()
+		{			
+			connection.Strings.Set(db, key, expectedString);
+			
+			subject.Save(key, "new value");
+			
+			var result = connection.Strings.GetString (db, key);
+			Assert.That (connection.Wait(result), Is.EqualTo("new value"));
+		}
+		
+		[Test]
+		public void should_get(){
+			connection.Strings.Set(db, key, expectedString);
+			
+			var result = subject.Get(key);
+			
+			Assert.That (result, Is.EqualTo(expectedString));
+		}
+		
+		[Test]
+		public void can_delete(){
+			connection.Strings.Set(db, key, expectedString);
+			
+			var result = connection.Strings.GetString(db,key);
+			Assert.That (connection.Wait(result), Is.EqualTo(expectedString));
+	
+			subject.Delete(key);
+			
+			var result2 = connection.Strings.GetString(db,key);
+			Assert.That (connection.Wait(result2), Is.EqualTo(null));
 		}
 	}
 }
